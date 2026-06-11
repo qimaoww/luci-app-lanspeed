c2a89037bd4b020f2789c8672e1051ec67d906c1
qimaoaa <qimaoaa@local>
2026-06-12 02:28:45 +0800

    lanspeed: 升级版本到 0.1.7

--- applications/luci-app-lanspeed/htdocs/luci-static/resources/lanspeed/version.js
+++ applications/luci-app-lanspeed/htdocs/luci-static/resources/lanspeed/version.js
@@ -2,7 +2,7 @@
 'require baseclass';
 
 return baseclass.extend({
-	PACKAGE_VERSION: '0.1.6',
+	PACKAGE_VERSION: '0.1.7',
 	PACKAGE_RELEASE: '1',
-	FULL_VERSION: '0.1.6-r1'
+	FULL_VERSION: '0.1.7-r1'
 });
